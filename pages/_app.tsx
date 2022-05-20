aaca35a25b1268dcb50d66cc118922e7a45df5a8
santech202 <santech202@gmail.com>
2022-05-20 12:31:08 +0300

    Update _app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -7,9 +7,9 @@ import '../styles/globals.scss'
 function MyApp({Component, pageProps}: AppProps) {
     return (
         <>
-            {/*<Head>*/}
-            {/*    <meta name="viewport" content="viewport-fit=cover" />*/}
-            {/*</Head>*/}
+            <Head>
+            <meta property="fb:pages" content="103265652394743" />
+            </Head>
             <AuthProvider>
                 <Component {...pageProps} />
                 <YMInitializer accounts={[88487475]} options={{webvisor: true}}/>

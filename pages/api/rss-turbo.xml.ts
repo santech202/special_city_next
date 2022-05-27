98f9756770955a1254ddeab9ad0b3adfa3d20bbf
santech202 <santech202@gmail.com>
2022-05-27 17:14:02 +0300

    commit

---
+++ pages/api/rss-turbo.xml.ts
@@ -0,0 +1,54 @@
+import {PostInterface} from "../../interfaces";
+import {getDynamicPaths} from "./rss.xml";
+
+// @ts-ignore
+const TR = require('turbo-rss');
+
+
+export default async (req: any, res: any) => {
+    const posts: PostInterface[] = await getDynamicPaths()
+
+    const related = posts.map((post) => ({
+        link: 'https://innoads.ru/post/' + post.slug,
+        image_url: post.preview,
+        text: post.title,
+        content: post.body
+    }))
+
+    const feed = new TR({
+        title: 'Доска объявлений города Иннополиса',
+        description: 'Доска объявлений – объявления города Иннополис о продаже и покупке товаров всех категорий. Самый простой способ продать или купить вещи',
+        link: 'https://innoads.ru'
+    });
+
+    feed.item({
+        title: 'InnoAds - доска объявлений города Иннополис',
+        image_url: 'https://innoads.ru/icons/icon-512x512.png',
+        url: 'https://innoads.ru',
+        author: 'InnoAds',
+        date: new Date().toString(),
+        content: '<p>hello</p>',
+        goals: [{
+            type: "yandex",
+            id: "turbo-goal-id",
+            counter_id: "88487475",
+            name: "post",
+        }],
+        menu: [{
+            link: 'https://innoads.ru/',
+            text: 'Главная'
+        }, {
+            link: 'https://innoads.ru/add',
+            text: 'Подать объявление'
+        }],
+        related
+    });
+
+    var xml = feed.xml();
+
+    res.writeHead(200, {
+        "Content-Type": "application/xml",
+    });
+
+    res.end(xml);
+}

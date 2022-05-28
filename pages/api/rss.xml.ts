1cfecd07a47d73bc6f2dd82a25567348bddd5ac6
santech202 <santech202@gmail.com>
2022-05-28 11:43:48 +0300

    Update rss.xml.ts

--- pages/api/rss.xml.ts
+++ pages/api/rss.xml.ts
@@ -9,7 +9,7 @@ const {Readable} = require("stream");
 
 export const getDynamicPaths = async () => {
     try {
-        const res = await axios.get(`${process.env.NEXT_PUBLIC_API_URL}/post?size=100`)
+        const res = await axios.get(`${process.env.NEXT_PUBLIC_API_URL}/post?size=1000`)
         const posts: PostInterface[] = res.data.content
         return posts
     } catch (e) {

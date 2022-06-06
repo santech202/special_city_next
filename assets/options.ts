8edde91c1cd90103b08280d0842ef0a4db2c244f
santech202 <santech202@gmail.com>
2022-06-06 08:21:02 +0300

    commit

--- assets/options.ts
+++ assets/options.ts
@@ -1,4 +1,4 @@
-type Label = "Продам" | "Одежда" | "Услуги" | "Недвижимость" | "Куплю" | "Вакансии"
+type Label = "Продам" | "Одежда" | "Услуги" | "Недвижимость" | "Куплю" | "Вакансии" | "Недвижим.."
 
 export interface OptionProps {
     value: number;
@@ -9,7 +9,19 @@ export const options: OptionProps [] = [
     {value: 1, label: "Продам"},
     {value: 2, label: "Куплю"},
     {value: 3, label: "Услуги"},
-    {value: 4, label: "Вакансии"},
+    // {value: 4, label: "Вакансии"},
     {value: 5, label: "Недвижимость"},
     {value: 6, label: "Одежда"}
+];
+
+interface CategoryProps extends OptionProps {
+    image: string
+}
+
+export const categories: CategoryProps [] = [
+    {value: 1, label: "Продам", image: '/images/sell.png'},
+    {value: 2, label: "Куплю", image: '/images/buy.png'},
+    {value: 3, label: "Услуги", image: '/images/services.png'},
+    {value: 5, label: "Недвижим..", image: '/images/estate.png'},
+    {value: 6, label: "Одежда", image: '/images/clothes.png'}
 ];

31f716e6f28c8221bbf7d8052b7147ea94e6415e
santech202 <santech202@gmail.com>
2022-06-10 18:13:26 +0300

    commit

--- functions/handleTelegramResponse.ts
+++ functions/handleTelegramResponse.ts
@@ -3,7 +3,9 @@ import axios from "axios";
 export const handleTelegramResponse = async (response: any, login: any) => {
     try {
         login(response)
-        return await axios.post(`${process.env.NEXT_PUBLIC_API_URL}/telegram`, response)
+        const res = await axios.post(`${process.env.NEXT_PUBLIC_API_URL}/telegram`, response)
+        console.log('res', res)
+        return res
     } catch (e) {
         console.log(e)
     }

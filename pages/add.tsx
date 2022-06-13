f180d82fe260838e7e2a841c5efd467d13af8e42
santech202 <santech202@gmail.com>
2022-06-13 16:41:40 +0300

    fix

--- pages/add.tsx
+++ pages/add.tsx
@@ -35,28 +35,12 @@ export default function Add() {
     const [loading, setLoading] = useState(false)
     const [sending, setSending] = useState(false)
 
-    const inputEl = useRef();
-
     const redirect = useCallback(async () => {
-        await router.push(routes.profile)
+        return await router.push(routes.profile)
     }, [router])
 
-    useEffect(() => {
-        const res = document.getElementById('hidden')
-        if (res && (!user || !user.username)) {
-            res.click()
-        }
-    }, [user])
-
-    if (!user || !user.username) {
-        return (
-            <MainLayout title={"Добавить объявление"}>
-                <div className={classes.center}>
-                    <Spinner/>
-                    <Button id='hidden' onClick={redirect}>Hidden</Button>
-                </div>
-            </MainLayout>
-        )
+    if (!user) {
+        redirect()
     }
 
     const onSubmit = async (data: any) => {

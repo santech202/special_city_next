b8ba501bf6f3dc8ef7f377451035d2eec48338c3
santech202 <santech202@gmail.com>
2022-06-14 07:10:53 +0300

    button interface

--- components/Button/Button.tsx
+++ components/Button/Button.tsx
@@ -1,11 +1,16 @@
-import React from "react";
 import cn from 'classnames';
+import React, {ReactNode} from "react";
 import classes from './Button.module.scss'
 
+interface ButtonProps {
+    children: ReactNode,
+    disabled?: boolean,
+    className?: string
+}
 
-export const Button = ({children, disabled, className, ...props}: any) => {
+export const Button = ({children, disabled, className, ...props}: ButtonProps) => {
     return (
-        <button className={cn(classes.button, className)} disable={disabled} {...props} >
+        <button className={cn(classes.button, className)} disabled={disabled} {...props} >
             {children}
         </button>
     )

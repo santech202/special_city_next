c3735f4f82fb77b4fe9d887b93bf608cb1d53185
santech202 <santech202@gmail.com>
2022-06-23 16:00:16 +0300

    refactor blog

--- pages/agreement.tsx
+++ pages/agreement.tsx
@@ -1,7 +1,7 @@
 import {MainLayout} from 'components/MainLayout/MainLayout';
 import MDX from 'components/MDX/agreement.mdx'
-import { serverSideTranslations } from 'next-i18next/serverSideTranslations';
-import { GetStaticProps } from 'next/types';
+import {serverSideTranslations} from 'next-i18next/serverSideTranslations';
+import {GetStaticProps} from 'next/types';
 import React from 'react';
 
 const Agreement = () => {
@@ -16,7 +16,7 @@ export default Agreement;
 export const getStaticProps: GetStaticProps = async ({locale}) => {
     return {
         props: {
-            ...(await serverSideTranslations(locale as string, ['common', 'footer'])),
+            ...(await serverSideTranslations(locale as string, ['common'])),
         },
     };
 }

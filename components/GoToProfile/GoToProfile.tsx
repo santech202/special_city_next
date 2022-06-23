0113fb2d73d0b206e4d885c5837c42bc4aee208e
santech202 <santech202@gmail.com>
2022-06-23 22:08:19 +0300

    commit

--- components/GoToProfile/GoToProfile.tsx
+++ components/GoToProfile/GoToProfile.tsx
@@ -1,17 +1,19 @@
 import Button from 'components/Button/Button';
 import {MainLayout} from 'components/MainLayout/MainLayout';
+import {useTranslation} from "next-i18next";
 import Link from 'next/link';
 import React from 'react';
 import classes from 'styles/classes.module.scss'
 import {routes, titles} from '../../constants';
 
 const GoToProfile = () => {
+    const {t} = useTranslation()
     return (
         <MainLayout title={titles.auth}>
             <div className={classes.center}>
-                <h2>Вы не авторизованы</h2>
+                <h2>{t('notAuthorized')}</h2>
                 <Link href={routes.profile} passHref>
-                    <Button>Перейти на страницу авторизации</Button>
+                    <Button>{t('goToAuth')}</Button>
                 </Link>
             </div>
         </MainLayout>

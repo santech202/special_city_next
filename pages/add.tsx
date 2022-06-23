80c93bb3cffca7f34f6f2040b048f22a5d6aabba
santech202 <santech202@gmail.com>
2022-06-23 10:11:08 +0300

    Update add.tsx

--- pages/add.tsx
+++ pages/add.tsx
@@ -87,7 +87,7 @@ export default function Add() {
             const chat_id = "@innoads";
             const category = options.find((item) => item.value == categoryValue) || options[0]
 
-            const text = `Категория: #${t(category.label)}\nЦена: ${price} ${title} \n\n${body} \n\nПодробнее: https://innoads.ru/post/${slugTitle} \n\nавтор: @${user.username}`;
+            const text = `Категория: #${t(category.label)}\nЦена: ${price} \n\n${title} \n\n${body} \n\nПодробнее: https://innoads.ru/post/${slugTitle} \n\nавтор: @${user.username}`;
 
             const sendPhoto = `https://api.telegram.org/bot${process.env.NEXT_PUBLIC_BOT_TOKEN}/sendMediaGroup?chat_id=${chat_id}`;
             const media = convertLinksToMedia(images, text);

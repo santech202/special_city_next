7d8fbce9c815c80321391fd3eabc60df1780fbea
santech202 <santech202@gmail.com>
2022-07-27 18:37:41 +0300

    Update options.ts

--- assets/options.ts
+++ assets/options.ts
@@ -23,6 +23,6 @@ export const categories: CategoryProps [] = [
     {value: 3, label: "services", image: '/images/services.png'},
     {value: 5, label: "estate", image: '/images/estate.png'},
     {value: 2, label: "buy", image: '/images/buy.png'},
-    // {value: 6, label: "clothes", image: '/images/clothes.png'},
-    {value: 7, label: "free", image: '/images/free.png'}
+    {value: 7, label: "free", image: '/images/free.png'},
+    {value: 6, label: "clothes", image: '/images/clothes.png'}
 ];

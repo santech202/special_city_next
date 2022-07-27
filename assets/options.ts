9730d8ee30afdb197b4e258fe8c2a4b20b1241ce
santech202 <santech202@gmail.com>
2022-07-27 18:35:27 +0300

    Update options.ts

--- assets/options.ts
+++ assets/options.ts
@@ -20,9 +20,9 @@ interface CategoryProps extends OptionProps {
 
 export const categories: CategoryProps [] = [
     {value: 1, label: "sell", image: '/images/sell.png'},
-    {value: 2, label: "buy", image: '/images/buy.png'},
     {value: 3, label: "services", image: '/images/services.png'},
     {value: 5, label: "estate", image: '/images/estate.png'},
-    {value: 6, label: "clothes", image: '/images/clothes.png'},
+    {value: 2, label: "buy", image: '/images/buy.png'},
+    // {value: 6, label: "clothes", image: '/images/clothes.png'},
     {value: 7, label: "free", image: '/images/free.png'}
 ];

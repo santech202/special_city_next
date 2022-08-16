0436b87722dcf57c32e4a2af32cacc8fe7366312
santech202 <santech202@gmail.com>
2022-08-16 21:32:38 +0300

    refactor delete turbo

--- pages/api/rss-turbo-delete.xml.ts
+++ pages/api/rss-turbo-delete.xml.ts
@@ -15,36 +15,14 @@ const RSSTurbo = async (req: NextApiRequest, res: NextApiResponse) => {
     });
 
     feed.item({
-        title: 'InnoAds - доска объявлений города Иннополис',
-        image_url: 'https://innoads.ru/icons/icon-512x512.png',
         url: 'https://innoads.ru',
-        author: 'InnoAds',
-        date: new Date().toString(),
-        content: 'Доска объявлений – объявления города Иннополис о продаже и покупке товаров всех категорий. Самый простой способ продать или купить вещи',
-        goals: [{
-            type: "yandex",
-            id: "turbo-goal-id",
-            counter_id: "88487475",
-            name: "post",
-        }],
-        menu: [{
-            link: 'https://innoads.ru/',
-            text: 'Главная'
-        }, {
-            link: 'https://innoads.ru/add',
-            text: 'Подать объявление'
-        }],
+        turboEnabled: false
     })
 
     posts.forEach((post) => {
         feed.item(
             {
-                title: post.title,
-                image_url: post.preview,
                 url: 'https://innoads.ru/post/' + post.slug,
-                author: 'https://t.me/' + post.telegram,
-                date: dayjs(post.createdAt).toDate(),
-                content: post.body,
                 turboEnabled: false
             }
         )

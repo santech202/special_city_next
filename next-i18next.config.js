00f9cae0156f9973f7768018ce8471b8580b6638
santech202 <santech202@gmail.com>
2022-11-19 23:31:52 +0300

    commit

--- next-i18next.config.js
+++ next-i18next.config.js
@@ -1,7 +1,10 @@
+const path = require("path");
+
 module.exports = {
     i18n: {
         defaultLocale: 'ru',
         locales: ['ru', 'en'],
     },
+    localePath: path.resolve("./locales"),
     trailingSlash: true,
 };

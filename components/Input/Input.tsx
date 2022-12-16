cbad632670ad6f01d1cb617367e313926472d715
santech202 <santech202@gmail.com>
2022-12-16 23:58:17 +0300

    add stylelint

--- components/Input/Input.tsx
+++ components/Input/Input.tsx
@@ -1,57 +1,33 @@
-import React, { ChangeEventHandler, forwardRef, HTMLInputTypeAttribute, ReactNode } from 'react'
+import React, { ChangeEventHandler, forwardRef } from 'react'
 import cn from 'classnames'
 
 import classes from './Input.module.scss'
 
 interface InputProp extends React.ComponentPropsWithoutRef<'input'> {
-    children?: ReactNode,
-    name?: string,
-    required?: boolean,
     register?: any,
-    type?: HTMLInputTypeAttribute,
-    placeholder?: string,
-    id?: string,
-    hidden?: boolean
     onChange?: (ChangeEventHandler<HTMLInputElement> | undefined) | any
     multiple?: boolean,
     accept?: string,
-    defaultValue?: string | number | string[] | undefined
-    value?: any,
-    style?: any,
-    className?: string
 }
 
 // eslint-disable-next-line react/display-name
 const Input = forwardRef<HTMLInputElement, InputProp>(({
-                   id,
-                   children,
-                   name,
-                   required,
-                   onChange,
-                   register,
-                   hidden = false,
-                   type = 'text',
-                   placeholder,
-                   multiple = false,
-                   accept,
-                   defaultValue,
-                   className,
-                   ...props
-               }, ref): JSX.Element => {
+                                                           name,
+                                                           onChange,
+                                                           register,
+                                                           className,
+                                                           children,
+                                                           required,
+                                                           ...props
+                                                       }, ref): JSX.Element => {
     const formRegister = register ? { ...register(name, { required }) } : null
     return (
-        <input id={id}
-               ref={ref}
-               hidden={hidden}
-               accept={accept}
-               type={type}
-               defaultValue={defaultValue}
-               onChange={onChange}
-               placeholder={placeholder}
-               className={cn(classes.input, className)}
-               multiple={multiple}
-               {...formRegister}
-               {...props}
+        <input
+            ref={ref}
+            onChange={onChange}
+            className={cn(classes.input, className)}
+            {...formRegister}
+            {...props}
 
         >
             {children}

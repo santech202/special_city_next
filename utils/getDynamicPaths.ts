08446a4c5ff9d662940c347b68ecd6b372791499
santech202 <santech202@gmail.com>
2023-01-10 11:22:16 +0400

    Update getDynamicPaths.ts

--- utils/getDynamicPaths.ts
+++ utils/getDynamicPaths.ts
@@ -3,7 +3,7 @@ import {PostInterface} from "interfaces";
 
 export const getDynamicPaths = async (count: number = 12) => {
     try {
-        const res = await axios.get(`${process.env.NEXT_PUBLIC_API_URL}/post?size=${count}`)
+        const res = await axios.get(`${process.env.NEXT_PUBLIC_API_URL}/posts?size=${count}`)
         const posts: PostInterface[] = res.data.content
         return posts
     } catch (e) {

44ac033ad4b84232556f153ea94be7825f1b2d30
santech202 <santech202@gmail.com>
2023-02-17 11:09:39 +0400

    Update [slug].tsx

--- pages/post/[slug].tsx
+++ pages/post/[slug].tsx
@@ -177,7 +177,7 @@ export const getStaticPaths: GetStaticPaths = async ({ locales = [] }) => {
         })))
     return {
         paths,
-        fallback: false,
+        fallback: 'blocking'
     }
 }
 

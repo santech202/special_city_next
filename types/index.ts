ed688eeef68431132057da54b820946cf55782cf
santech202 <santech202@gmail.com>
2023-02-18 22:44:46 +0400

    use local tg user interface

--- types/index.ts
+++ types/index.ts
@@ -1,4 +1,12 @@
-import { TelegramUser } from 'telegram-login-button'
+export interface TelegramUser {
+    id: number;
+    first_name: string;
+    last_name: string;
+    username: string;
+    photo_url: string;
+    auth_date: number;
+    hash: string;
+}
 
 export type UserProps = Pick<TelegramUser, 'id' | 'username'>
 
